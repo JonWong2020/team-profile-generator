a0c60f40e03533cd05debe72dd1851ad77e4832c
JonWong2020 <jonathannwong50@aol.com>
2022-09-14 22:47:50 -0700

    fixed generated sample

--- src/generatePage.js
+++ src/generatePage.js
@@ -1,7 +1,7 @@
 const roleTitles = {
-    "Engineer": {value: "github", render: "Github"},
-    "Intern": {value: "school", render: "School"},
-    "Manager": {value: "officeNumber", render: "Office #"}
+    "Engineer": {value: 'github', render: "Github"},
+    "Intern": {value: 'school', render: "School"},
+    "Manager": {value: 'officeNumber', render: "Office #"}
 }
 
 const getGeneratedWorkerInfo = (role, data) => {
@@ -29,10 +29,11 @@ const getTemplate = data => {
         <header class="p-5">
             <h1>My Team</h1>
         </header>
-        <div class="d-flex justify-content-center flex-row flex-wrap w-50 p-3">
+        <div class="d-flex justify-content-center flex-row flex-wrap p-3">
             ${data}
         </div>
         
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
     </body>
     </html>
